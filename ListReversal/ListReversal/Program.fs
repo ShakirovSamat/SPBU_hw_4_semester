c2a2baa31660d120edbaf3dec73ebae825cc1504
ShakirovSamat <samatx-r@mail.ru>
2024-02-18 18:16:04 +0300

    added solution

---
+++ ListReversal/ListReversal/Program.fs
@@ -0,0 +1,11 @@
+﻿let reverseList list = 
+    let rec inner list acc =
+        if list = [] then
+            acc
+        else
+            inner (List.tail list) (List.head list::acc)
+    inner list []
+
+let list = [1;2;3;4;5;5]
+
+printfn $"%A{reverseList list}"
