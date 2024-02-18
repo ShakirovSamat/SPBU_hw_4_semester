adc834cc2b79c7ae8f1cd94c2414e1f13523a62e
ShakirovSamat <samatx-r@mail.ru>
2024-02-18 18:52:06 +0300

    added solution

---
+++ Factorial/Factorial/Program.fs
@@ -0,0 +1,10 @@
+﻿let factorial n = 
+    let rec inner number acc =
+        if acc = n then
+            number
+        else
+            inner (number * acc) (acc + 1)
+    inner 1 1
+
+printfn $"%A{factorial 10}"
+            
