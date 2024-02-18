654f35364ef0d97d44c681030837c45c6699f9cc
ShakirovSamat <samatx-r@mail.ru>
2024-02-18 18:43:10 +0300

    added solution

---
+++ FibonacciNumbers/FibonacciNumbers/Program.fs
@@ -0,0 +1,9 @@
+﻿let rec getFibonacchiNumber n =
+    let rec inner list acc =
+        if n = acc then
+            List.head list
+        else
+            inner (list[0] + list[1]::list) (acc + 1)
+    inner [1;0] 2
+
+printfn $"%A{getFibonacchiNumber 45}"
