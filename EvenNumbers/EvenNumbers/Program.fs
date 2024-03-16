cf13fa39464eebd8bd62c705494f5b5c64529894
ShakirovSamat <samatx-r@mail.ru>
2024-03-16 15:50:11 +0300

    feat: added solution

---
+++ EvenNumbers/EvenNumbers/Program.fs
@@ -0,0 +1,22 @@
+﻿module Program
+
+let isEven number = 
+    if number % 2 = 0 then
+        1
+    else
+        0
+
+let getAmountOfEvenNumbersByMapFunction list =
+    list
+    |> List.map isEven
+    |> List.sum
+
+let getAmountOfEvenNumbersByFilterFunction list =
+    list
+    |> List.filter (fun x -> x % 2 = 0)
+    |> List.length
+
+let getAmountOfEvenNumbersByFoldFunction list =
+    (0, list)
+    ||> List.fold (fun s v -> s + isEven v)
+
---
+++ EvenNumbers/EvenNumbers/Tests.fs
@@ -0,0 +1,9 @@
+﻿open Program
+open FsCheck
+
+let evenNumberTest list = 
+    getAmountOfEvenNumbersByMapFunction list  = getAmountOfEvenNumbersByFilterFunction list &&
+    getAmountOfEvenNumbersByFilterFunction list = getAmountOfEvenNumbersByFoldFunction list &&
+    getAmountOfEvenNumbersByFoldFunction list = getAmountOfEvenNumbersByMapFunction list
+    
+Check.Quick evenNumberTest
